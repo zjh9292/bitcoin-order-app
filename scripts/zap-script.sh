3ff01911fd315df89bd5ed2db42d91f998160f9f
unknown <jinhaofruity@gmail.com>
2024-07-16 16:03:59 +0800

    lint, dast, sast

--- scripts/zap-script.sh
+++ scripts/zap-script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker pull owasp/zap2docker-stable
-docker run -i owasp/zap2docker-stable zap-baseline.py -t "https://zjh9292.github.io/bitcoin-order-app/" -l PASS > zap_baseline_report.html
+docker pull zaproxy/zap-stable
+docker run -i zaproxy/zap-stable zap-baseline.py -t "https://zjh9292.github.io/bitcoin-order-app/" -l PASS > zap_baseline_report.html
 
 echo $? > /dev/null
